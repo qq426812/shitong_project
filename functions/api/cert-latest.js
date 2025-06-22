5859f5ad1b4d3204e5b711d85fe965101677ca0c
qq426812 <d426812d@gmail.com>
2025-06-22 21:52:02 +0800

    Update cert-latest.js

--- functions/api/cert-latest.js
+++ functions/api/cert-latest.js
@@ -1,5 +1,13 @@
 export async function onRequestGet({ env }) {
   const stmt = env.DB.prepare("SELECT id FROM certificates ORDER BY id DESC LIMIT 1");
   const result = await stmt.first();
-  return new Response(JSON.stringify({ success: true, ids: [result.id]  }), { headers: { 'Content-Type': 'application/json' } });
+  return new Response(
+    JSON.stringify({ success: true, id: result?.id || null }),
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+  );
 }

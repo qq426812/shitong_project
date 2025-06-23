21cb2ba5b3a1faa564a22643db26ef276d0b6563
qq426812 <d426812d@gmail.com>
2025-06-23 10:49:25 +0800

    Update cert-update.js

--- functions/api_1/cert-update.js
+++ functions/api_1/cert-update.js
@@ -2,12 +2,12 @@ export async function onRequestPost({ request, env }) {
   try {
     const {
       id,
-      certificate_unit,
-      instrument_name,
-      serial_number,
-      asset_number,
-      calibration_date,
-      certificate_number
+      certificate_number,   // 证书编号
+      certificate_unit,     // 证书单位
+      calibration_date,     // 校准日期
+      instrument_name,      // 仪器名称
+      serial_number,        // 出厂编号
+      asset_number          // 管理编号
     } = await request.json();
 
     if (!id) {
@@ -19,23 +19,23 @@ export async function onRequestPost({ request, env }) {
 
     const stmt = env.DB.prepare(`
       UPDATE cert_lookup SET
+        certificate_number = ?,
         certificate_unit = ?,
+        calibration_date = ?,
         instrument_name = ?,
         serial_number = ?,
-        asset_number = ?,
-        calibration_date = ?,
-        certificate_number = ?
+        asset_number = ?
       WHERE id = ?
     `);
 
     await stmt
       .bind(
+        certificate_number,
         certificate_unit,
+        calibration_date,
         instrument_name,
         serial_number,
         asset_number,
-        calibration_date,
-        certificate_number,
         id
       )
       .run();

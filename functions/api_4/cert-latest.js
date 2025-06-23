1bb875884953858df4741d25cafb0da9ddc73c4a
qq426812 <d426812d@gmail.com>
2025-06-23 20:35:34 +0800

    Update cert-latest.js

--- functions/api_4/cert-latest.js
+++ functions/api_4/cert-latest.js
@@ -1,5 +1,5 @@
 export async function onRequestGet({ env }) {
-  const stmt = env.DB.prepare("SELECT id FROM certificates3 ORDER BY id DESC LIMIT 1");
+  const stmt = env.DB.prepare("SELECT id FROM certificates_simple ORDER BY id DESC LIMIT 1");
   const result = await stmt.first();
   return new Response(
     JSON.stringify({ success: true, id: result?.id || null }),
